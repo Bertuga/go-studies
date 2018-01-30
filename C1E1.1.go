567c88ee3f89e5f424c2b1307e3efa36247d293f
Bruno Bertuga <bertuga@gmail.com>
2018-01-30 14:26:18 -0200

    Exercise 1.1: Create a loop with the for construct. Make it loop 10 times and print out the loop counter with the fmt package.

---
+++ C1E1.1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//Create a loop with the for construct. Make it loop 10 times and print out the loop counter with the fmt package.
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
