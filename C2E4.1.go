3f4d577cc214f70825528a2e7774c5010485be8a
Bruno Bertuga <bertuga@gmail.com>
2018-01-31 14:44:52 -0200

    Exercise 4.1: Write a function that returns a function that performs a +2 on integers. Name the function plusTwo.

---
+++ C2E4.1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//Exercise 4.1: Write a function that returns a function that performs a +2 on integers. Name the function plusTwo.
+func main() {
+	p := plusTwo()
+	fmt.Println(p(2))
+}
+
+func plusTwo() func(int) int {
+	return func(i int) int { return i + 2 }
+}
