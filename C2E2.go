2b87f6115c6b407ee2e8ac17847927755796799f
Bruno Bertuga <bertuga@gmail.com>
2018-01-31 14:20:36 -0200

    Exercise 2: Write a function that takes an int value and gives that many terms of the Fibonacci sequence.

---
+++ C2E2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//Exercise 2: Write a function that takes an int value and gives that many terms of the Fibonacci sequence.
+func main() {
+	i := 10
+	fmt.Println(fibonacci(i))
+}
+
+func fibonacci(i int) []int {
+	terms := make([]int, i)
+	for k, _ := range terms {
+		switch k {
+		case 0, 1:
+			terms[k] = 1
+		default:
+			terms[k] = terms[k-1] + terms[k-2]
+		}
+	}
+	return terms
+}
