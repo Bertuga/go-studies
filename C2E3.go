b2f0cae9ce761cd4ee70ca2378d63db030d73b4a
Bruno Bertuga <bertuga@gmail.com>
2018-01-31 14:24:31 -0200

    Exercise 3: Write a function that takes a variable number of ints and print each integer on a separate line.

---
+++ C2E3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//Exercise 3: Write a function that takes a variable number of ints and print each integer on a separate line.
+func main() {
+	print_ints(1, 3, 6, 8)
+	print_ints(2, 44, 66, 43, 0, 22)
+}
+
+func print_ints(ints ...int) {
+	for _, v := range ints {
+		fmt.Println(v)
+	}
+}
