1b33a526e5d6aaa3598575d4c2e528d4ac85a003
Bruno Bertuga <bertuga@gmail.com>
2018-02-07 13:53:22 -0200

    Exercise 1.2: Write a simple unit test for this package. You should at least test that a Pop works after a Push.

---
+++ src/stack/stack_test.go
@@ -0,0 +1,34 @@
+package stack
+
+import "testing"
+
+//Exercise 1.2: Write a simple unit test for this package. You should at least test that a Pop works after a Push.
+func TestPush(t *testing.T) {
+	stack := new(Stack)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	stack.Push(5)
+	stack.Push(6)
+	stack.Push(7)
+}
+
+func TestPop(t *testing.T) {
+	stack := new(Stack)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	if stack.Pop() != 3 {
+		t.Log("First out should be 3!")
+		t.Fail()
+	}
+	if stack.Pop() != 2 {
+		t.Log("Second out should be 2!")
+		t.Fail()
+	}
+	if stack.Pop() != 1 {
+		t.Log("Last out should be 1!")
+		t.Fail()
+	}
+}
